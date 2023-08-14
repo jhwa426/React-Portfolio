b5b58cdd509013d2903259bf2ec7942baad06b32
Jeff Hwang <wodud6359@gmail.com>
2023-08-15 10:56:09 +1200

    sidebar

--- React-Portfolio/website/src/components/Sidebar/index.js
+++ React-Portfolio/website/src/components/Sidebar/index.js
@@ -1,28 +1,60 @@
+import React, { useState } from 'react';
+
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+
+import LogoJ from '../../assets/images/letter-j.png';
+import LogoSubtitle from '../../assets/images/Jeff_Hwang.png';
+
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 
 const Sidebar = () => {
     return (
         <div className='nav-bar'>
             <Link className='logo' to='/'>
-                <img scr={LogoS} alt='Logo' />
-                <img className="sub-logo  " scr={LogoSubtitle} alt='slobodan' />
+                {/* <img src={require('../../assets/images/letter-j.png')} alt='Logo' /> */}
+                <img scr={LogoJ} alt='Logo' />
+                <img className="sub-logo" scr={LogoSubtitle} alt='slobodan' />
             </Link>
 
             <nav>
                 <NavLink exact="true" activeclassname="active" to="/">
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
             </nav>
 
+            <ul>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/jeffhwa411/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://github.com/jhwa426?tab=repositories'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.facebook.com/wodud6359'>
+                        <FontAwesomeIcon icon={faFacebook} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+
         </div>
-    )
+    );
 
 }
 
