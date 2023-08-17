0de1cac8e85af2923f8be241a84eba5ccaeb56f5
Jeff Hwang <wodud6359@gmail.com>
2023-08-18 09:57:37 +1200

    layout

--- React-Portfolio/website/src/components/Layout/index.js
+++ React-Portfolio/website/src/components/Layout/index.js
@@ -1,10 +1,25 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () => {
     return (
-        <div>
+        <div className='App'>
             <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>
+                    &lt;body&gt;
+                    <Outlet />
+                    <span className='tas bottom-tags'>
+                        &lt;body&gt;
+                        <br />
+                        <span className='bottom-tags-html'>
+                            &lt;/html&gt;
+                        </span>
+                    </span>
+                </span>
+
+            </div>
         </div>
     );
 }
