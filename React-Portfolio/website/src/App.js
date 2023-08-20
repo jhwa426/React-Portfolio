18dec6c05e663ec66745b3d3eb591c61f0174693
Jeff Hwang <wodud6359@gmail.com>
2023-08-20 17:54:48 +1200

    logo

--- React-Portfolio/website/src/App.js
+++ React-Portfolio/website/src/App.js
@@ -10,6 +10,7 @@ function App() {
 		<Routes>
 			<Route path="/" element={<Layout />} >
 				<Route index element={<Home />} />
+				<Route path='about' element={<Home />} />
 			</Route>
 		</Routes>
 	);
