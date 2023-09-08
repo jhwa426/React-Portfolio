ac80c7e7da6de6771de7a6a25e4a55ee63d77544
Jeff Hwang <wodud6359@gmail.com>
2023-09-08 16:05:08 +1200

    React-JS-Personal-Portfolio-Website-ver3

--- React-JS-Personal-Portfolio-Website-ver3/personal-website/src/App.js
+++ React-JS-Personal-Portfolio-Website-ver3/personal-website/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './utils/Themes';
+
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      Starting - Set up
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Body>
+        HI
+      </Body>
+    </ThemeProvider>
   );
 }
 
