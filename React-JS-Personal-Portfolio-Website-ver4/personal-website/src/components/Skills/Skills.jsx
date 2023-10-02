10712cb7a07ac08923bbbca618948279f189d226
Jeff Hwang <wodud6359@gmail.com>
2023-10-02 18:42:27 +1300

    Skills_v4

--- React-JS-Personal-Portfolio-Website-ver4/personal-website/src/components/Skills/Skills.jsx
+++ React-JS-Personal-Portfolio-Website-ver4/personal-website/src/components/Skills/Skills.jsx
@@ -2,9 +2,212 @@ import './Skills.css';
 
 const Skills = () => {
     return (
-        <section id="Skills">
-            This is Skills
-        </section>
+        <div id="Skills">
+            <h2 className="skills__title">Skills</h2>
+
+            <div className="container skills__container">
+
+                <div className="skills__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="skills__content">
+                        <article className="skill__details">
+                            <img className="skills__img" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="React JS" />
+                            <div>
+                                <h4>React JS</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg" alt="Redux" />
+                            <div>
+                                <h4>Redux</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=" alt="Next JS" />
+                            <div>
+                                <h4>Next JS</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="HTML" />
+                            <div>
+                                <h4>HTML</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-plain.svg" alt="CSS" />
+                            <div>
+                                <h4>CSS</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="JavaScript" />
+                            <div>
+                                <h4>JavaScript</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg" alt="Bootstrap" />
+                            <div>
+                                <h4>Bootstrap</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg" alt="" />
+                            <div>
+                                <h4>Material UI</h4>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="skills__backend_and_database">
+                    <h3>Backend & Database</h3>
+                    <div className="skills__content">
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-plain-wordmark.svg" alt="Node JS" />
+                            <div>
+                                <h4>Node JS</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="Express JS" />
+                            <div>
+                                <h4>Express JS</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB" />
+                            <div>
+                                <h4>MongoDB</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://upload.wikimedia.org/wikipedia/de/8/8c/Microsoft_SQL_Server_Logo.svg" alt="MSSQL" />
+                            <div>
+                                <h4>MSSQL</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sqlite/sqlite-original.svg" alt="SQLite" />
+                            <div>
+                                <h4>SQLite</h4>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="skills__object_oriented_programming">
+                    <h3>Object-Oriented Programming</h3>
+                    <div className="skills__content">
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="Java" />
+                            <div>
+                                <h4>Java</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="C#" />
+                            <div>
+                                <h4>C#</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" alt=".NET" />
+                            <div>
+                                <h4>.NET</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://lh3.googleusercontent.com/-XvJzhz3pfH0/XjYG_xWkESI/AAAAAAAAJ9c/AYlgAtRknEU2W5fMcFhQoL6rmO8EBtIDQCK8BGAsYHg/s0/2020-02-01.png" alt="REST API" />
+                            <div>
+                                <h4>REST API</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://cdn.worldvectorlogo.com/logos/agile-software.svg" alt="Agile Methodology" />
+                            <div>
+                                <h4>Agile Methodology</h4>
+                            </div>
+                        </article>
+
+                    </div>
+                </div>
+
+                <div className="skills__data_science_and_tools">
+                    <h3>Data Science & Tools</h3>
+                    <div className="skills__content">
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="Python" />
+                            <div>
+                                <h4>Python</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/pandas/pandas-original.svg" alt="Pandas" />
+                            <div>
+                                <h4>Pandas</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/numpy/numpy-original.svg" alt="NumPy" />
+                            <div>
+                                <h4>NumPy</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/r/r-original.svg" alt="R" />
+                            <div>
+                                <h4>R</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://cdn.worldvectorlogo.com/logos/tableau-software.svg" alt="Tableau" />
+                            <div>
+                                <h4>Tableau</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" />
+                            <div>
+                                <h4>GitHub</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667" alt="Git" />
+                            <div>
+                                <h4>Git</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png" alt="Netlify" />
+                            <div>
+                                <h4>Netlify</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519" alt="VS Code" />
+                            <div>
+                                <h4>VS Code</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667" alt="Postman" />
+                            <div>
+                                <h4>Postman</h4>
+                            </div>
+                        </article>
+                        <article className="skill__details">
+                            <img className="skills__img" src="https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667" alt="Figma" />
+                            <div>
+                                <h4>Figma</h4>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
